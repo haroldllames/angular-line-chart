f1dbd0a13aa9da0b804729557c08cbfa613ddf6a
dveloper_harold <haroldllames2800@gmail.com>
2017-11-14 15:08:35 +0800

    Update app.component.ts
    another commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent implements OnInit {
 
   public datasets = [
     {
-      label: "Monthly Rate",
+      label: "Monthly Rate for 2016",
       data: [this.January, this.February, this.March, this.April, this.May, this.June]
     }
   ];
